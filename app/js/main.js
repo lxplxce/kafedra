d75d3a547f06a0848b9db9bce2039b4a4bba2305
lxplxce <unfort33@mail.ru>
2023-02-14 22:44:09 +0700

    wtf

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 // $(function () {
 //     $('body').css('background-color', 'purple')
-=======
+
 // $(function () {
 //     $('body').css('background-color', 'purple')
->>>>>>> 8c4ad0f40b73fbd15d2830862515c2bf87469f08
+
 // })
